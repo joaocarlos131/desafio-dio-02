c7f75c4a1033c8f3314d3ac44e1c9b0706d3c4b5
joaocarlos131 <joaocarlos-131@hotmail.com>
2023-12-08 20:51:02 -0300

    mensagem adicionada

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ function rankeadas(vitorias, derrotas){
     return saldoRankeadas
 }
 
-saldoVitorias = rankeadas(5, 10)
-console.log(saldoVitorias)
+saldoVitorias = rankeadas(95, 20)
 
-if (saldoVitorias <= 10){
-    console.log("Ferro")
+if ((saldoVitorias >= 0) && (saldoVitorias <= 10)){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Ferro")
 } else if((saldoVitorias >= 11) && (saldoVitorias <= 20)){
-    console.log("Bronze")
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Bronze")
+} else if((saldoVitorias >= 21) && (saldoVitorias <= 50)){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Prata")
+} else if((saldoVitorias >= 51) && (saldoVitorias <= 80)){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Ouro")
+} else if((saldoVitorias >= 81) && (saldoVitorias <= 90)){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Diamante")
+} else if((saldoVitorias >= 91) && (saldoVitorias <= 100)){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Lendário")
+} else if(saldoVitorias >= 101){
+    console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de Imortal")
 } else {
     console.log("Nivel não encontrado")
 }
+    
